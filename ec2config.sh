8c547acc476074a4a8a05139e917a44852f65e3d
Ravi Bhagyoday <44194596+RaviBhagyoday@users.noreply.github.com>
2020-05-09 02:29:32 +0530

    Create ec2config.sh

---
+++ ec2config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo su
+yum install httpd php php-mysql -y
+yum update -y
+chkconfig httpd on
+service httpd start
+cd /var/www/html
+echo “java developer zone is a knowledge sharing platform” > index.php
+wget https://ktexpertsbucket.s3.ap-south-1.amazonaws.com/connect.php
